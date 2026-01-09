18037de546f4d17d8118f182ff3a43471734efb6
Dark01-Techie-Invader <kimt62376@gmail.com>
2026-01-09 20:02:29 +0500

    Create auth_service.dart

---
+++ auth_service.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign Up
+  Future<User?> signUp(String email, String password) async {
+    UserCredential result = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return result.user;
+  }
+
+  // Sign In
+  Future<User?> signIn(String email, String password) async {
+    UserCredential result = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return result.user;
+  }
+
+  // Sign Out
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+}
